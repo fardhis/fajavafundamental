ccd2f040e8808bba2ac8e81f9f33be8ce1d968f1
Fadillah Ardhi <f.ardhi.77@gmail.com>
2023-06-26 21:55:21 +0700

    Java Fundamental On IntelliJ

--- src/main/java/com/juaracoding/fajavafundamental/testharian/praktikum1/Praktikum1.java
+++ src/main/java/com/juaracoding/fajavafundamental/testharian/praktikum1/Praktikum1.java
@@ -13,31 +13,49 @@ import java.util.Scanner;
 
 public class Praktikum1 {
     public static void main(String[] args) {
+//        (input) Input Kata : Jquery 1995@v1.0
+//        (output) Hasil : 529
+//        (input) Input Kata :Bebas Lepas@
+//        (output) Hasil : 238
+//        Pertanyaan untuk input Kata Tid4k $@ma adalah?
 
                 Scanner scanner = new Scanner(System.in);
-        String strInput = "";
-        int intOutput = 0;
-        char chInput = 'a';
-        int intConvert = 0;
+        String strInput = ""; //VARIABEL PENAMPUNG INPUT USER
+        int intOutput = 0; // PENAMBAHAN UNTUK HASIL AKHIR
+        char chInput = 'a'; // UNTUK MENAMPUNG KARAKTER HASIL LOOP
+        int intConvert = 0; //VARIABEL UNTUK CONVERT CHAR KE KODE ASCII
 
+        //      NILAI ASCI HURUF KAPITAL 65-90
+        //      NILAI ASCI HURUF KECIL 97-122
+        //      NILAI ASCI ANGKA 48-57
+        //      NILAI ASCI SIMBOL1 32-47
+        //      NILAI ASCI SIMBOL2 58-64
+
+        //STEP1
         System.out.println("Masukkan Kata: ");
         strInput = scanner.nextLine();
 
+        //STEP
         for (int n=0;n<strInput.length();n++)
         {
+            //STEP3
             chInput = strInput.charAt(n);
+            //STEP4
             intConvert = chInput;
 
+            //STEP5
             if (
                     (intConvert>=32 && intConvert<=47 ) ||
                             (intConvert>=58 && intConvert<=64)||
                             (intConvert>=48 && intConvert<=57)||
-                            (intConvert>=65 && intConvert<=90)
+                            (intConvert>=65 && intConvert<=90) //RANGE ASCI HURUF KECIL RIDAK DIPAKAI
             )
             {
+                //STEP6
                 intOutput = intOutput + intConvert;
             }
         }
+        //STEP AKHIR
         System.out.println("Output dari input "+strInput+" adalah "+intOutput);
 
     }
